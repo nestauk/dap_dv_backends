50b1b36f3310d563c8132ea9ff2814e224308c61
doogyb <samueldoogan@gmail.com>
2022-04-01 15:38:24 +0100

    Trigger automatic snapshot before annotation (#14)
    This PR covers the need to create snapshots before and after the
    annotation process, in case the user needs to roll back changes or
    the process fails, or the mapping has been changed and the only way
    to reverse it is to restore a snapshot from before the annotation
    process began
    
    closes #8

--- arxlive_spotlight_annotation/src/bin/es/annotate.mjs
+++ arxlive_spotlight_annotation/src/bin/es/annotate.mjs
@@ -2,13 +2,15 @@ import * as cliProgress from 'cli-progress';
 import { Command } from 'commander';
 
 import { scroll, clearScroll } from 'es/search.mjs';
-import { count } from 'es/index.mjs';
+import { count, getMappings, updateMapping } from 'es/index.mjs';
+import { register, trigger, status } from 'es/snapshot.mjs';
 import {
 	annotateDocument,
 	uploadAnnotatedDocument,
 } from 'dbpedia/spotlight.mjs';
 import { arxliveCopy } from 'conf/config.mjs';
 import { promisesHandler, batch } from '../../node_modules/util.mjs';
+import { settings, defaultMapping } from 'conf/config.mjs';
 
 const program = new Command();
 program.option(
@@ -37,20 +39,62 @@ program.option(
 	'Number of pages to iterate over',
 	'all'
 );
-program.option(
+program.requiredOption(
 	'-f, --field <field>',
 	'Field of doc to be used as input text for annotation'
 );
-
+program.option(
+	'-n, --name <annotated_field_name>',
+	'Name of new field to be created'
+);
+program.option(
+	'--force',
+	'Force the annotation process, even if no snapshots can be created'
+);
 program.parse();
 const options = program.opts();
 
 const bar = new cliProgress.SingleBar(
-	{ etaBuffer: options.pageSize * 10 },
+	{ etaBuffer: options.size * 10 },
 	cliProgress.Presets.shades_classic
 );
 
 const main = async () => {
+	if (!settings.snapshotSettings && !options.force) {
+		throw new Error(
+			'No snapshot configuration found and force flag not supplied'
+		);
+	}
+
+	const newFieldName = options.name || `dbpedia_entities-${options.field}`;
+	const currentMapping = await getMappings(options.domain, options.index);
+	if (newFieldName in currentMapping[options.index].mappings.properties) {
+		throw new Error(
+			'Field already exists at index mapping, and force flag or continue flag not supplied'
+		);
+	}
+
+	const { body: snapshotStatus } = await status(options.domain);
+	if (snapshotStatus.snapshots.length !== 0) {
+		throw new Error(
+			`Can't trigger a snapshot as domain is already busy creating one`
+		);
+	}
+
+	// initialize snapshot repository with given settings
+	await register(options.domain, settings.snapshotSettings.repository);
+	await trigger(
+		options.domain,
+		settings.snapshotSettings.repository,
+		`${newFieldName.toLowerCase()}-before-${+new Date()}`
+	);
+
+	// create mapping for new field
+	const mappingPayload = { properties: { [newFieldName]: defaultMapping } };
+	await updateMapping(options.domain, options.index, {
+		payload: mappingPayload,
+	});
+
 	const totalDocuments = await count(options.domain, options.index);
 	bar.start(totalDocuments, 0);
 
@@ -77,6 +121,7 @@ const main = async () => {
 					return uploadAnnotatedDocument(
 						doc.annotations,
 						doc.document._id,
+						newFieldName,
 						options.domain,
 						options.index
 					);
@@ -86,6 +131,13 @@ const main = async () => {
 	}
 	bar.stop();
 	clearScroll(options.domain);
+
+	// trigger snapshot after successful run
+	await trigger(
+		options.domain,
+		settings.snapshotSettings.repository,
+		`${newFieldName.toLowerCase()}-after-${+new Date()}`
+	);
 };
 
 main();
