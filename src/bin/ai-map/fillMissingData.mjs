5771bc0acfd85c029715f4596fc891e7fb29a8ab
doogyb <samueldoogan@gmail.com>
2022-05-09 15:17:44 +0100

    Bing search for missing data (#99)
    Introduces a module to access the Bing search API which will replace
    missing data for the AI map.
    
    closes #98

---
+++ src/bin/ai-map/fillMissingData.mjs
@@ -0,0 +1,42 @@
+import { promises as fs } from 'fs';
+
+import * as _ from 'lamb';
+import { Presets, SingleBar } from 'cli-progress';
+
+import { search } from 'bing/search.mjs';
+
+const bar = new SingleBar(Presets.rect);
+
+const FILE_SOURCE = 'data/ai-map/data/ai_map_orgs_places.json';
+const FILE_SUBSET = './data/ai-map/data/ai_map_orgs_places_populated_subset.json';
+const FILE_POPULATED = './data/ai-map/data/ai_map_orgs_places_populated.json';
+
+const main = async () => {
+	const data = JSON.parse(await fs.readFile(FILE_SOURCE));
+	const [ filled, missing ] = _.partition(data.orgs, org => org.url);
+	const found = [];
+
+	bar.start(missing.length, 0);
+	// loop needed to avoid rate limiting issues
+	for (const org of missing) {
+		const query = `"${org.name}"`;
+		// eslint-disable-next-line no-await-in-loop
+		const searchResults = await search(query);
+		const [ topResult ] = searchResults.webPages.value;
+		found.push({
+			...org,
+			url: topResult.url,
+			description: topResult.snippet
+		});
+		bar.increment();
+	}
+	bar.stop();
+	const filledData = {
+		...data,
+		orgs: [ ...filled, ...found ]
+	};
+	await fs.writeFile(FILE_SUBSET, JSON.stringify(found, null, 4));
+	await fs.writeFile(FILE_POPULATED, JSON.stringify(filledData, null, 4));
+};
+
+main();
