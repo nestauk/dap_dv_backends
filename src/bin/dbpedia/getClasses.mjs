c540e4711f5581373c80f6bef03eaf3726a0d9a4
doogyb <samueldoogan@gmail.com>
2022-06-20 17:00:39 +0100

    Add data file

--- src/bin/dbpedia/getClasses.mjs
+++ src/bin/dbpedia/getClasses.mjs
@@ -3,9 +3,13 @@ import { promises as fs } from 'fs';
 import { Command, InvalidArgumentError } from 'commander';
 import * as _ from 'lamb';
 
+import { stringify } from '@svizzle/utils';
 import { arxliveCopy } from 'conf/config.mjs';
 import { getClasses } from 'dbpedia/requests.mjs';
+import { dbo, dbr, owl } from 'dbpedia/util.mjs';
 import { getEntities } from 'es/entities.mjs';
+import { batchIterate } from 'util/array.mjs';
+import { saveObj } from '@svizzle/file';
 
 const program = new Command();
 program.option(
@@ -38,10 +42,34 @@ const main = async () => {
 		? await getEntities(options.index)
 		: JSON.parse(await fs.readFile(options.path));
 
-	const sample=titles.slice(100, 110);
-	console.log(sample);
-	console.log(await getClasses(sample, { depth: 10, squash: true, fullURI:true }));
-
+	const results = await batchIterate(
+		titles,
+		batch => getClasses(batch, { fullURI: true, squash: false }),
+		{ concat: false }
+	);
+	const classes = _.reduce(results, (acc, curr) => ({...acc, ...curr}));
+	const classesWithPrefix = JSON.parse(
+		stringify(classes)
+		.replaceAll(dbr, 'dbr:')
+		.replaceAll(dbo, 'dbo:')
+		.replaceAll(owl, 'owl:')
+	);
+	const sortedClasses = _.mapValues(
+		classesWithPrefix,
+		_.sortWith([_.getKey('depth')])
+	);
+	const sortedAndSquashedClasses = _.mapValues(
+		sortedClasses,
+		_.mapWith(_.getKey('class_'))
+	);
+	const save = (path, object) => saveObj(path, 4)(object);
+	save(
+		options.out,
+		{
+			prefixes: { dbo, dbr, owl },
+			classes: sortedAndSquashedClasses
+		}
+	);
 };
 
 main();
