4f55a966697a9977067d8cfd6672c14826ffcb7c
doogyb <samueldoogan@gmail.com>
2022-08-30 16:31:48 +0100

    Script to generate graph csv from DBpedia entities (#141)
    Creates a csv for the nodes created when annotating entities and their
    DBpedia abstracts.
    Also includes cypher script to ingest the resulting csv data into
    a neo4j database.
    
    closes #140

---
+++ src/bin/es/graph.mjs
@@ -0,0 +1,51 @@
+import { createWriteStream } from 'fs';
+
+import { Presets, SingleBar } from 'cli-progress';
+import { Command } from 'commander';
+import { stringify } from 'csv-stringify/sync';
+import * as _ from 'lamb';
+
+import { arxliveCopy } from 'conf/config.mjs';
+import { count } from 'es/index.mjs';
+import { scroll, clearScroll } from 'es/search.mjs';
+
+const program = new Command();
+
+program
+.command('csv')
+.description('Creates a csv from an ES index representing a DBpedia entities graph.')
+.argument('<inputIndex>', 'Input index name')
+.argument('<outputPath>', 'Path to output the csv.')
+.argument('[domain]', 'domain on which to create index', arxliveCopy)
+.action(async (index, path, domain) => {
+	const writer = createWriteStream(path);
+	const scroller = scroll(domain, index, { size: 1000, });
+	const total = await count(domain, index);
+	const bar = new SingleBar(Presets.rect);
+
+	const headers = ['sourceNode', 'abstractNode', 'confidence'];
+	writer.write(stringify([headers]));
+	bar.start(total, 0);
+
+	let page;
+	for await (page of scroller) {
+		const nodes = _.flatMap(
+			page.hits.hits,
+			({ _source: doc }) => {
+			    const edges = _.map(
+				    doc.dbpedia_entities || [],
+				    entity => [doc.URI, entity.URI, entity.confidence]
+			    );
+			    return edges;
+			}
+		);
+		bar.increment(page.hits.hits.length);
+		writer.write(stringify(nodes));
+	}
+	if (page) {
+		clearScroll(domain, page._scroll_id);
+	}
+	bar.stop();
+});
+
+program.parse();
