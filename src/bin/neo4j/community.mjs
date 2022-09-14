20f0808c41e996d26d8218bcf487b76930880c70
doogyb <samueldoogan@gmail.com>
2022-09-14 15:59:48 +0100

    Neo4j SDK community detection (#153)
    closes #149

---
+++ src/bin/neo4j/community.mjs
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+
+import { stringify } from 'csv-stringify/sync';
+import { Command } from 'commander';
+import * as _ from 'lamb';
+
+import { drop, project } from 'neo4j/gds.mjs';
+import { stream } from 'neo4j/community.mjs';
+import { commanderParseInt } from 'util/commander.mjs';
+
+const program = new Command();
+program.requiredOption(
+	'--threshold <value>',
+	'threshold to filter entities with',
+	commanderParseInt
+);
+
+program.requiredOption('--path', 'path for outputs');
+program.parse();
+const options = program.opts();
+const main = async () => {
+
+	const path = `${options.path}/threshold_${options.threshold}`;
+	const graphName = `${options.threshold}_entities`;
+
+	const opts = { maxIterations: 100, includeIntermediateCommunities: true };
+	await project(graphName, options.threshold);
+	const { data, metadata } = await stream(graphName, opts);
+
+	if (!fs.existsSync(path)) {
+		fs.mkdirSync(path, { recursive: true });
+	}
+	fs.writeFileSync(`${path}/communities.csv`, stringify(data, { header: true }));
+	fs.writeFileSync(`${path}/communities.json`, JSON.stringify(data, null, 2));
+	fs.writeFileSync(`${path}/metadata.json`, JSON.stringify(metadata, null, 2));
+
+	await drop(graphName);
+};
+
+main();
