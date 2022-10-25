16f032502e53ae881dd2e15645c5a71e40a972a6
doogyb <samueldoogan@gmail.com>
2022-10-25 12:02:04 +0100

    Integrate s3 logic, expose as endpoint (#182)
    Refactors existing s3 bucket scripts indexToBucket and bucketToIndex
    such that the logic is available from the generic node_modules dir.
    This then allows us to expose an `/annotate/s3` endpoint for the
    annotation service which will be used to annotate buckets. The
    logic being refactored in this PR is used to copy the contents of
    an s3 bucket to an ES index before then annotating as usual. Once
    the annotation has finished, we copy the index back to the specified
    output bucket.
    
    closes #179

--- src/bin/s3/bucketToIndex.mjs
+++ src/bin/s3/bucketToIndex.mjs
@@ -1,10 +1,8 @@
 import { Command, Option } from 'commander';
 import * as _ from 'lamb';
 
-import { streamArray, streamObject } from 'aws/s3.mjs';
+import { bucketToIndex } from 'aws/s3.mjs';
 import { arxliveCopy } from 'conf/config.mjs';
-import { bulkRequest } from 'es/bulk.mjs';
-import { createIndex } from 'es/index.mjs';
 import { commanderParseInt } from 'util/commander.mjs';
 
 const program = new Command();
@@ -16,12 +14,18 @@ program.option(
 program.option(
 	'-c, --chunk-size <bytes>',
 	'number of bytes to chunk with',
-	commanderParseInt
+	commanderParseInt,
+	512_000,
+);
+program.option(
+	'-f, --id-field <field>',
+	'Name of field to use as id',
+	null
 );
 program.addOption(
 	new Option('--format <format>')
 	.choices(['array', 'object'])
-	.default('object')
+	.default('array')
 );
 program.requiredOption('-i, --index <index>', 'name of index');
 program.requiredOption('-b, --bucket <bucket name>', 'name of s3 bucket');
@@ -29,38 +33,17 @@ program.requiredOption('-k, --key <bucket key>', 'name of s3 key');
 program.parse();
 
 const options = program.opts();
-const formatObject = _.pipe([
-	_.pairs,
-	_.mapWith(([key, value]) => ({ _id: key, data: { value } }))
-]);
-const formatArray = _.mapWith(
-	({ id, ...rest }) => ({ _id: id, data: rest })
-);
-
-const funcs = {
-	'object': [streamObject, formatObject],
-	'array': [streamArray, formatArray]
-};
 
 const main = async () => {
-
-	await createIndex(options.index, options.domain);
-	const [stream, formatter] = funcs[options.format];
-	const streamer = stream(
+	await bucketToIndex(
+		options.index,
+		options.domain,
 		options.bucket,
 		options.key,
-		{ increment: options.chunkSize }
+		options.idField,
+		options.format,
+		options.chunkSize
 	);
-
-	for await (let docs of streamer) {
-		const bulkFormat = formatter(docs);
-		await bulkRequest(
-			options.domain,
-			options.index,
-			bulkFormat,
-			'create'
-		);
-	}
 };
 
 await main();
--- src/services/annotation/service/routes.mjs
+++ src/services/annotation/service/routes.mjs
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
+import { bucketToIndex, indexToBucket } from 'aws/s3.mjs';
 import { arxliveCopy as DEFAULT_DOMAIN } from 'conf/config.mjs';
 import { count } from 'es/index.mjs';
 
@@ -7,6 +8,7 @@ import { MAX_WORKERS, annotationEndpoint } from '../config.mjs';
 import { annotationService } from './machine.mjs';
 import { Progress } from './progress.mjs';
 import { state } from './state.mjs';
+import { parseS3URI, uriToEsIndex } from './util.mjs';
 
 
 export const routes = (fastify, options, done) => {
@@ -24,6 +26,53 @@ export const routes = (fastify, options, done) => {
 			reply.send(state.progress[id].status());
 		}
 		reply.code(404).send({error: 'no annotation with that id found.'});
+	});
+
+	fastify.post('/annotate/s3', async (request, reply) => {
+
+		const domain = DEFAULT_DOMAIN;
+		let {
+			field,
+			email,
+			s3_input_uri,
+			s3_output_uri,
+			includeMetaData = true,
+			newField = 'dbpedia_entities',
+			workers = MAX_WORKERS,
+		} = request.body;
+
+		const id = uuidv4();
+		reply.send({ id });
+
+		const { bucket: inBucket, key: inKey } = parseS3URI(s3_input_uri);
+		const { bucket: outBucket, key: outKey } = parseS3URI(s3_output_uri);
+
+		const index = uriToEsIndex(s3_input_uri);
+		await bucketToIndex(
+			index,
+			domain,
+			inBucket,
+			inKey,
+		);
+
+		const total = await count(domain, index);
+		const progress = new Progress(total, email, id, index, domain, outBucket, outKey);
+		state.progress[id] = progress;
+
+		annotationService.send(
+			{
+				id,
+				workers,
+				domain,
+				index,
+				field,
+				newField,
+				annotationEndpoint,
+				includeMetaData,
+				progress,
+				type: 'PROVISION',
+			}
+		);
 	});
 
 	fastify.post('/annotate/es', async (request, reply) => {
