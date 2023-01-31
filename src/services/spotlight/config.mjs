ab829dbb7943595acf802e112e793586b558701d
doogyb <samueldoogan@gmail.com>
2023-01-31 11:42:06 +0000

    Port Terraform configuration to service (#220)
    Ports the configuration for setting up nodes on AWS to the service,
    instead of keeping this information in the utils package, which is
    intended to be more generic and serve only utilities.
    
    closes #219

--- src/services/spotlight/config.mjs
+++ src/services/spotlight/config.mjs
@@ -8,3 +8,29 @@ export const PORT = 3000;
 export const WORKER_PORT = 4000;
 export const SERVER_DIRECTORY = path.join(__dirname, '..');
 export const TERRAFORM_DIRECTORY = path.join(SERVER_DIRECTORY, 'terraform');
+
+export const ami = 'ami-06cb614d0f047d106';
+export const spotlightInstanceType = 't2.xlarge';
+
+export const scaffold = {
+	provider: [
+		{
+			aws: {
+				region: 'eu-west-2',
+			},
+		},
+	],
+	terraform: [
+		{
+			required_providers: [
+				{
+					aws: {
+						source: 'hashicorp/aws',
+						version: '~\u003e 4.16',
+					},
+				},
+			],
+			required_version: '\u003e= 1.2.0',
+		},
+	],
+};
