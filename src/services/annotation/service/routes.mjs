b35fbf799222d6327dd221d96604cad6284bbb61
doogyb <samueldoogan@gmail.com>
2023-06-28 15:30:24 +0100

    Change default annotation ES domain (#235)
    fixes #234

--- src/services/annotation/service/routes.mjs
+++ src/services/annotation/service/routes.mjs
@@ -2,10 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { sendEmail } from 'dap_dv_backends_utils/aws/email.mjs';
 import { bucketToIndex, indexToBucket } from 'dap_dv_backends_utils/aws/s3.mjs';
-import { arxliveCopy as DEFAULT_DOMAIN } from 'dap_dv_backends_utils/conf/config.mjs';
 import { count } from 'dap_dv_backends_utils/es/index.mjs';
 
-import { MAX_WORKERS, internalAnnotationEndpoint } from '../config.mjs';
+import { MAX_WORKERS, internalAnnotationEndpoint, dapAnnotationDomain as DEFAULT_DOMAIN } from '../config.mjs';
 import { parseBasicAuth } from './auth.mjs';
 import { annotationService } from './machine.mjs';
 import { Progress } from './progress.mjs';
