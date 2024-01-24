d4874a283854e3c9bf0a6c3868d9703ba0b8ee3b
Luca Bonavita <mindrones@users.noreply.github.com>
2024-01-24 15:28:49 +0000

    DOCKER_SPOTLIGHT_ENDPOINT -. DOCKER_ANNOTATION_NODE_ENDPOINT

--- src/services/annotationMapper/config.mjs
+++ src/services/annotationMapper/config.mjs
@@ -2,4 +2,4 @@ export const PORT = 4000;
 
 // the odd looking domain 'worker' is created when launching docker containers.
 // see /src/services/spotlight/spotlightDockerCommand.sh
-export const DOCKER_SPOTLIGHT_ENDPOINT = 'http://worker:2222/rest/annotate';
+export const DOCKER_ANNOTATION_NODE_ENDPOINT = 'http://worker:2222/rest/annotate';
