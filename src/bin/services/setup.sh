125d3fcd4dfa5d0d952950338443e034e1fba8f7
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2024-02-22 15:06:44 -0300

    spaces to tabs

--- src/bin/services/setup.sh
+++ src/bin/services/setup.sh
@@ -1,22 +1,22 @@
 if [ -z "$REPO_DIR_PATH" ]; then
-    echo "REPO_DIR_PATH is not set or is empty."
-    exit 1
+	echo "REPO_DIR_PATH is not set or is empty."
+	exit 1
 fi
 if [ -z "$CERTBOT_EMAIL" ]; then
-    echo "CERTBOT_EMAIL is not set or is empty."
-    exit 1
+	echo "CERTBOT_EMAIL is not set or is empty."
+	exit 1
 fi
 if [ -z "$AWS_USERNAME" ]; then
-    echo "AWS_USERNAME is not set or is empty."
-    exit 1
+	echo "AWS_USERNAME is not set or is empty."
+	exit 1
 fi
 if [ -z "$AWS_REGION" ]; then
-    echo "AWS_REGION is not set or is empty."
-    exit 1
+	echo "AWS_REGION is not set or is empty."
+	exit 1
 fi
 if [ -z "$1" ]; then
-    echo "Please provide one of these ANNOTATION, API, AUTHENTICATION, PROVISION as a parameter."
-    exit 1
+	echo "Please provide one of these ANNOTATION, API, AUTHENTICATION, PROVISION as a parameter."
+	exit 1
 fi
 
 echo "******************************************************"
@@ -33,23 +33,23 @@ echo "******************************************************"
 ./src/bin/services/setup-nginx.sh $1
 
 if [ "$1" = "AUTHENTICATION" ]; then
-    echo "******************************************************"
-    echo "${REPO_DIR_PATH}/src/bin/services/install-docker.sh"
-    echo "******************************************************"
+	echo "******************************************************"
+	echo "${REPO_DIR_PATH}/src/bin/services/install-docker.sh"
+	echo "******************************************************"
 
-    ./src/bin/services/install-docker.sh
-    sudo docker compose -f src/services/authentication/docker-compose.yml up -d
+	./src/bin/services/install-docker.sh
+	sudo docker compose -f src/services/authentication/docker-compose.yml up -d
 fi
 
 if [ "$1" = "PROVISION" ]; then
-    echo "******************************************************"
-    echo "${REPO_DIR_PATH}/src/bin/services/install-terraform.sh"
-    echo "******************************************************"
+	echo "******************************************************"
+	echo "${REPO_DIR_PATH}/src/bin/services/install-terraform.sh"
+	echo "******************************************************"
 
-    aws s3 cp s3://dap-dv-backends/spotlight.pem ~/.ssh/spotlight.pem
-    chmod 600 ~/.ssh/spotlight.pem
+	aws s3 cp s3://dap-dv-backends/spotlight.pem ~/.ssh/spotlight.pem
+	chmod 600 ~/.ssh/spotlight.pem
 
-    ./src/bin/services/install-terraform.sh
+	./src/bin/services/install-terraform.sh
 
-    ./src/bin/utils/create-iam-keys.sh $AWS_USERNAME $AWS_REGION
+	./src/bin/utils/create-iam-keys.sh $AWS_USERNAME $AWS_REGION
 fi
--- src/bin/utils/wait-for-dns.sh
+++ src/bin/utils/wait-for-dns.sh
@@ -5,12 +5,12 @@ DOMAIN_NAME=$1
 INSTANCE_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 
 while true; do
-DNS_RESOLVED_IP=$(dig +short $DOMAIN_NAME)
-if [[ "$DNS_RESOLVED_IP" == "$INSTANCE_IP" ]]; then
-    echo "DNS has propagated with IP $INSTANCE_IP"
-    break
-else
-    echo "Waiting for DNS to propagate..."
-    sleep 30
-fi
+	DNS_RESOLVED_IP=$(dig +short $DOMAIN_NAME)
+	if [[ "$DNS_RESOLVED_IP" == "$INSTANCE_IP" ]]; then
+		echo "DNS has propagated with IP $INSTANCE_IP"
+		break
+	else
+		echo "Waiting for DNS to propagate..."
+		sleep 30
+	fi
 done
